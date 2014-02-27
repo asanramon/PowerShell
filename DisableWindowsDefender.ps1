c7af2af2bb9186aa8c4f7890a47b3628d98ff353
Angelo San Ramon <AngeloSanRamon@gmail.com>
2014-02-27 10:34:25 -0800

    deleted DisableWindowsDefender.ps1

--- DisableWindowsDefender.ps1
+++
@@ -1,7 +0,0 @@
-﻿#Author: Angelo San Ramon
-#Date: June 8, 2013
-#Purpose: Script to disable Windows Defender Service.
-
-#Create a scheduled task running under "NT Authority\System" account to disable Windows Defender service.
-$action = New-ScheduledTaskAction -Execute "net stop windefend"
-$trigger = New-ScheduledTaskTrigger
